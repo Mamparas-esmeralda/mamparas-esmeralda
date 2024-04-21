2bda31b5e17d9e720606b5b1508441fd3415bdd9
Juan Peñalba <juantandil123@gmail.com>
2024-04-21 14:27:38 -0300

    logica menu creada

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SECTIONS = [
+  { title: "Mamparas", url: "url('/public/images/mamparas.jpg')" },
+  { title: "Barandas y balcones", url: "url('/public/images/barandas.jpg')" },
+  { title: "Doble vidrio hermetico", url: "url('/public/images/dvh.jpg')" },
+  { title: "Procesado", url: "mamparas" },
+];
