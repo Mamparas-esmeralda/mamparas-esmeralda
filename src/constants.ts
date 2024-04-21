472a45f243d81daf637410261dac76cd242b0697
Juan Peñalba <juantandil123@gmail.com>
2024-04-21 17:47:53 -0300

    rutas creadas

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SECTIONS = [
-  { title: "Mamparas", url: "url('/public/images/mamparas.jpg')" },
-  { title: "Barandas y balcones", url: "url('/public/images/barandas.jpg')" },
-  { title: "Doble vidrio hermetico", url: "url('/public/images/dvh.jpg')" },
-  { title: "Procesado", url: "mamparas" },
+  { title: "mamparas", url: "url('/public/images/mamparas.jpg')" },
+  { title: "barandas y balcones", url: "url('/public/images/barandas.jpg')" },
+  { title: "doble vidrio hermetico", url: "url('/public/images/dvh.jpg')" },
+  { title: "procesado", url: "url('/public/images/procesado.jpg')" },
 ];
